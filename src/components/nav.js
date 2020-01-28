0878ef54f79e8df32edd00a2b3175f26f379d707
JosephIbe <youngjoey242@gmail.com>
2020-01-28 19:55:05 +0530

    Initial Scaffold and Housekeeping

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components'
+
+function Nav(){
+    return (
+        <div>
+            Navbar
+        </div>
+    );
+}
+
+// const NavBar = styled.div
